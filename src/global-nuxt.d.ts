a743537bfd555c6eaf6e926a9ec9ad998482efd7
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-26 14:51:51 -0500

    refactor: delete useState shim
    We will explicitly import nuxt things going forward, to help consumer
    libraries plug in

--- src/global-nuxt.d.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from 'vue'
-
-// global-nuxt.d.ts
-declare global {
-  // Provide the minimal signature you need
-  function useState<T>(key: string, init?: () => T): Ref<T>
-}
-
-export {}
